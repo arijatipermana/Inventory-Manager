d7cc8f0a980abbc78a962e99971f17022cf9e21d
Easy Code <easycode441@gmail.com>
2020-10-11 10:09:25 +0700

    Menambahkan file header.php

---
+++ template/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock Manager</title>
+    <link rel="shortcut icon" href="dist/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body>
+    <!-- navbar -->
+    <nav id="main-navbar" class="navbar navbar-expand-md navbar-dark bg-dark py-0">
+        <div class="container">
+            <span class="navbar-brand">Hello, Admin</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="tampil_barang.php" class="nav-link p-3 active">Tabel Barang</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
